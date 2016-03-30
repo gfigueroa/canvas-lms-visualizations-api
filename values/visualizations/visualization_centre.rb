fb211102a9ad1cf9dca0119f64b444d2c1dc4aef
James Uanhoro <stonegold546@gmail.com>
2016-03-30 15:25:58 +0800

    Minor refactoring

--- values/visualizations/visualization_centre.rb
+++ values/visualizations/visualization_centre.rb
@@ -5,12 +5,8 @@ NAME = 'View'
 class VisualizationTraffic
   def initialize(route, data)
     @data = JSON.parse(data, quirks_mode: true)
-    @object =
-    if route.include? UNDERSCORE
-      object = route.split(UNDERSCORE).map(&:capitalize).join
-      Object.const_get "#{object}#{NAME}"
-    else Object.const_get "#{route.capitalize}#{NAME}"
-    end.new(@data)
+    object = route.split(UNDERSCORE).map(&:capitalize).join
+    @object = Object.const_get("#{object}#{NAME}").new(@data)
   end
 
   def call
